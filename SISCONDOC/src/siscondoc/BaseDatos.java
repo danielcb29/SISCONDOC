43fef8724c1b359cdd3f48e23d04d9fcbe3410d6
danielcb29 <danielc.web@gmail.com>
2014-10-21 22:09:31 -0500

    Clase base de datos para acceso a psql

---
+++ SISCONDOC/src/siscondoc/BaseDatos.java
@@ -0,0 +1,14 @@
+package siscondoc;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author daniel
+ */
+public class BaseDatos {
+    
+}
