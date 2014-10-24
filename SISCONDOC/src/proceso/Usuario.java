dbae85062948f69eb9d51bb1cf70d74eec20fa75
danielcb29 <danielc.web@gmail.com>
2014-10-23 21:41:47 -0500

    Clase Usuario creada sin convocatoria (se deja comentado atributo y metodos para la convocatoria)

---
+++ SISCONDOC/src/proceso/Usuario.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proceso;
+
+/**
+ *
+ * @author daniel
+ */
+public class Usuario {
+    /*
+     * Elements of class Usuario which es an abstraction of person who will uses the system 
+     * Atributos necesarios para que la clase exista 
+     */
+    private String name;
+    private String lastName;
+    private String userName;
+    private String password;
+    
+    /*
+     * One user has one "Convocatoria" 
+     */
+    //private Convocatoria convocatoria;
+    
+    public Usuario(String nameUser , String lastNameUser , String userName , String passwordUser){
+        name = nameUser;
+        lastName = lastNameUser;
+        this.userName = userName;
+        password=passwordUser;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public String getUserName(){
+        return userName;
+    }
+    public String getPassword(){
+        return password;
+    }
+    
+    public void setUserName(String newUserName){
+        userName = newUserName; 
+    }
+    public void setPassword(String newPassword){
+        password=newPassword;
+    }
+    /*
+     * One user can be on multiple "Convocatoria" in a time frame
+     * 
+     * public void setConvocatoria(Convocatoria newConvocatoria){
+     *      convocatoria = newConvocatoria;
+     * }
+     */
+    
+            
+            
+    
+}
