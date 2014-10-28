123f353f4a72c8b31190c55ee0599cc78ce5646a
danielcb29 <danielc.web@gmail.com>
2014-10-27 19:59:08 -0500

    Archivos de nelson funcionando v1.0

--- SISCONDOC/src/presentacion/VistaAdmin.java
+++ SISCONDOC/src/presentacion/VistaAdmin.java
@@ -177,12 +177,12 @@ public class VistaAdmin extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    /*public static void main(String args[]) {
         // Set the Nimbus look and feel 
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+         */ /*
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -207,7 +207,7 @@ public class VistaAdmin extends javax.swing.JFrame {
                 new VistaAdmin().setVisible(true);
             }
         });
-    }
+    }*/
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btCrearConvocatoria;
