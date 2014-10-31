633337cbbfcb4d40459e1604cd75809b8acd91ea
danielcb29 <danielc.web@gmail.com>
2014-10-31 18:13:34 -0500

    anandido atributo estado con su respectivo get y set , se inicializa en true

--- SISCONDOC/src/proceso/Convocatoria.java
+++ SISCONDOC/src/proceso/Convocatoria.java
@@ -11,9 +11,11 @@ package proceso;
 public class Convocatoria {
     private String name;
     private String description;
+    private boolean state;
     public Convocatoria(String paramName , String paramDescrip){
         name = paramName;
         description = paramDescrip; 
+        state = true;
     }
     public String getName(){
         return name;
@@ -21,10 +23,16 @@ public class Convocatoria {
     public String getDescription(){
         return description;
     }
+    public boolean getState(){
+        return state;
+    }
     public void setName(String newName){
         name = newName;
     }
     public void setDescription(String newDescrip){
         description = newDescrip;
+    }
+    public void setState(boolean newState){
+        state = newState;
     }
 }
