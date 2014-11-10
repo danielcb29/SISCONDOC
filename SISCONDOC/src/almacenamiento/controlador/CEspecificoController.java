2665fa261a810fab0efe7366a9416268796f15ef
brayanrodbajo <bryan951112@gmail.com>
2014-11-10 16:01:47 -0500

    Update CEspecificoController.java

--- SISCONDOC/src/almacenamiento/controlador/CEspecificoController.java
+++ SISCONDOC/src/almacenamiento/controlador/CEspecificoController.java
@@ -20,18 +20,18 @@ public class CEspecificoController {
     }
     /**
         * crear o agregar un conocimiento específico a la tabla.
-        * @param ce la cedula del aspirante al que se le agrega el CEspecifico
+        * @param ce el CEspecifico a agregar del aspirante.
         * @param cedula la cedula del aspirante
         * @return 1 en caso que se cumpla todo en su flujo normal, -3 en caso de que el perfil del usuario este malo, -2 en caso de un error de integridad referencial o de entidad, y -1 en cualquier otro caso.
     */
     public int createCEspecifico(CEspecifico[] ce, String cedula){
-        int size = ce.length;
+        
         int result = 0;
 
-        for(int i = 0; i< size ; i++){
-            result = daoCe.createCEspecifico(ce[i],cedula);
+       
+        result = daoCe.createCEspecifico(ce[i],cedula);
             
-        }
+       
         
         return result;
     }
